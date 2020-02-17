fffc46ec66314d23f74aa643517c3a3242dee291
Ingrid Amaral <guid_95@hotmail.com>
2020-02-17 15:15:35 +0100

    user input

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import SearchBar from './SearchBar'
+
+class App extends React.Component {
+
+    handleSubmit(term) {
+        console.log(term)
+    }
+
+    render() {
+        return (
+            <div className='ui container' style={{marginTop: '1rem'}}>
+                <SearchBar 
+                  onSubmit={ this.handleSubmit } />
+            </div>
+        )
+    }      
+};
+
+export default App;
